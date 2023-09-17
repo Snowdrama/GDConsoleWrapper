63f05a55ce37cfbb080e36b025282c005e36567c
John 'Snow' Close <SnowdramaGames@gmail.com>
2023-09-17 04:16:48 -0400

    Added functions with params

--- GDConsole.cs
+++ GDConsole.cs
@@ -1,6 +1,3 @@
-
-
-
 using System;
 using Godot;
 
@@ -13,11 +10,61 @@ public partial class GDConsole : Node
         instance = this;
         consoleRef = GetNode("/root/Console");
     }
+
+    //0 params
     public static void AddCommand(string command, Action callable){
         instance.LinkCommandToConsole(command, callable);
     }
-
     public void LinkCommandToConsole(string command, Action callable){
 		consoleRef.Call("add_command", command, Callable.From(callable));
+    }
+    public static void RemoveCommand(string command, Action callable){
+        instance.UnlinkCommandToConsole(command, callable);
+    }
+    public void UnlinkCommandToConsole(string command, Action callable){
+		consoleRef.Call("remove_command", command, Callable.From(callable));
+    }
+
+    //1 param
+    public static void AddCommand<T>(string command, Action<T> callable){
+        instance.LinkCommandToConsole(command, callable, 1);
+    }
+    public void LinkCommandToConsole<T>(string command, Action<T> callable, int paramCount = 0){
+		consoleRef.Call("add_command", command, Callable.From(callable), 1);
+    }
+    public static void RemoveCommand<T>(string command, Action<T> callable){
+        instance.UnlinkCommandToConsole(command, callable, 1);
+    }
+    public void UnlinkCommandToConsole<T>(string command, Action<T> callable, int paramCount = 0){
+		consoleRef.Call("remove_command", command, Callable.From(callable), 1);
+    }
+
+    
+    //2 params
+    public static void AddCommand<T, I>(string command, Action<T, I> callable){
+        instance.LinkCommandToConsole(command, callable, 2);
+    }
+    public void LinkCommandToConsole<T, I>(string command, Action<T, I> callable, int paramCount = 0){
+		consoleRef.Call("add_command", command, Callable.From(callable), 2);
+    }
+    public static void RemoveCommand<T, I>(string command, Action<T, I> callable){
+        instance.UnlinkCommandToConsole(command, callable, 2);
+    }
+    public void UnlinkCommandToConsole<T, I>(string command, Action<T, I> callable, int paramCount = 0){
+		consoleRef.Call("remove_command", command, Callable.From(callable), 2);
+    }
+
+    //3 params
+    public static void AddCommand<T, I, J>(string command, Action<T, I, J> callable){
+        instance.LinkCommandToConsole(command, callable, 3);
+    }
+    public void LinkCommandToConsole<T, I, J>(string command, Action<T, I, J> callable, int paramCount = 0){
+		consoleRef.Call("add_command", command, Callable.From(callable), 3);
+    }
+    public static void RemoveCommand<T, I, J>(string command, Action<T, I, J> callable){
+        instance.UnlinkCommandToConsole(command, callable, 3);
+    }
+    public void UnlinkCommandToConsole<T, I, J>(string command, Action<T, I, J> callable, int paramCount = 0){
+		consoleRef.Call("remove_command", command, Callable.From(callable), 3);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # GDConsoleWrapper
-A C# wraper for jitspoe's Godot Console
+This is a C# wrapper for jitspoe's Godot Console: 
+https://github.com/jitspoe/godot-console
+
+This is intended just to be a wrapper for the console written in C# for mixed codebases that
+have some C# and some GDScript and want both to be able to use the console. 
+
+For a full reimplementation of the console in C# check out this written by MolikoDeveloper
+https://github.com/MolikoDeveloper/Csharp-Console-Godot/tree/main/addons/csharpConsole
+
+# Usage
+If you haven't, First install the godot console from [jitspoe's Repo](https://github.com/jitspoe/godot-console)
+Drop the `GDConsoleWrapper` folder into the `addons` folder the structure should look like: `addons/GDConsoleWrapper` 
+Then enable both the godot-console and GDConsoleWrapper in the project settings under plugins.
+
+You should be all set! Leave an issue if you run into any problems!
+
+# Version
+Quick note this was built on 4.1.1 and while it might work with earlier versions I've only tested on 4.1.1
+
+# How To Use
+In your code you can use it by calling AddCommand like this:
+```cs
+public partial class SomeNode : Node{
+    int hp = 100;
+    public override void _Ready()
+    {
+        GDConsole.AddCommand<int>("SetHP", SetHP)
+    }
+
+    public void SetHP(int hp){
+        this.hp = hp;
+    }
+}
+```
+Note how you declare the type in the generic parameter. This supports up to 3 parameters:
+```cs
+public partial class SomeNode : Node{
+    int hp = 100;
+    public override void _Ready()
+    {
+        //this takes 3 params! 
+        GDConsole.AddCommand<int, string, bool>("SetHP", DoSomething)
+    }
+
+    public void DoSomething(int someInt, string someString, bool someBool){
+        //whatever
+    }
+}
+```
